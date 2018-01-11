c7fb4c3bbfa4b8b0c74cc1ad842b73070ecacac9
Valentin <valentin_github@abv.bg>
2018-01-11 13:27:55 +0200

    Update

---
+++ BgCars.Services/Models/Blog/BlogArticleDetailsServiceModel.cs
@@ -0,0 +1,32 @@
+﻿namespace BgCars.Services.Models.Blog
+{
+    using AutoMapper;
+    using Common.Mapping;
+    using Data.Models;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    using static Data.DataConstants;
+
+    public class BlogArticleDetailsServiceModel : IMapFrom<Article>, IHaveCustomMapping
+    {
+        public int Id { get; set; }
+
+        [MinLength(ArticleTitleMinLenght)]
+        [MaxLength(ArticleTitleMaxLenght)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ThumbnailUrl { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public string Author { get; set; }
+
+        public void ConfigureMapping(Profile mapper)
+            => mapper
+                .CreateMap<Article, BlogArticleDetailsServiceModel>()
+                .ForMember(a => a.Author, cfg => cfg.MapFrom(a => a.Author.UserName));
+    }
+}
